d89f0877211c0d8794ed8036ca5b912960a4a062
matheusjanuario <matheusmcj18@hotmail.com>
2021-05-05 22:30:16 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,32 @@
+import BarChart from "components/BarChart";
+import DonutChart from "components/DonutChart";
 import DataTable from "components/DataTable";
 import Footer from "components/Footer";
 import NavBar from "components/NavBar";
 
-// Representa um componentes do Frontend React, que é informado no arquivo Index.tsx
 function App() {
   return (
     <>
       <NavBar />
-      <div className = "container">
-        <h1 className="text-primary">Olá Mundo!</h1>
+      <div className="container">
+        <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
-        <DataTable />
-      </div>
-      <Footer />
+          <div className="row px-3">
+            <div className="col-sm-6">
+            < h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+              <BarChart/>
+            </div>
+            <div className="col-sm-6">
+              < h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+              <DonutChart/>
+            </div>
+          </div>
+<div className="py-3">
+<h2 className="text-primary">Todas vendas</h2>
+</div>
+          <DataTable />
+          </div>
+          <Footer />
     </>
   );
 }

34b714ed1bdc5e74148b153113183dbec44aa985
matheusjanuario <matheusmcj18@hotmail.com>
2021-05-05 21:51:44 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,19 @@
+import DataTable from "components/DataTable";
+import Footer from "components/Footer";
+import NavBar from "components/NavBar";
 
+// Representa um componentes do Frontend React, que é informado no arquivo Index.tsx
 function App() {
   return (
-    <div>
-      <h1 className = "text-primary">Olá Mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className = "container">
+        <h1 className="text-primary">Olá Mundo!</h1>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+// Representa um componentes do Frontend React, que é informado no arquivo Index.tsx
+
+const Footer = () => {
+    return (
+
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/matheusjanuario" target="_blank" rel="noreferrer">Matheus Januario</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br />
+            Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;

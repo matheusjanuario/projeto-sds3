9f664af9339c37fc24342aa21fb986de74d92b54
matheusjanuario <matheusmcj18@hotmail.com>
2021-05-09 15:29:13 -0300

    Routes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,11 +1,12 @@
+//local onde configuramos as dependencias dos componentes 
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';//Importado caminho do CSS do bootstrap
 import 'assets/css/styles.css';
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
+ReactDOM.render( //Trecho onde adicionamos os componentes React que seram apresentados após criação "App" ex de componente.
+  <React.StrictMode> 
     <App />
   </React.StrictMode>,
   document.getElementById('root')
